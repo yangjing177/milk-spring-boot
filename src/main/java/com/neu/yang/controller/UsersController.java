34eaea2bce69215fb58ea9a99ce634e09fd74b07
yang <374743964@qq.com>
2019-05-07 20:22:08 +0800

    新增管理员退出功能接口

--- src/main/java/com/neu/yang/controller/UsersController.java
+++ src/main/java/com/neu/yang/controller/UsersController.java
@@ -1,5 +1,6 @@
 package com.neu.yang.controller;
 
+import com.neu.yang.entity.Info;
 import com.neu.yang.model.Goods;
 import com.neu.yang.model.Users;
 import com.neu.yang.service.UsersService;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.Map;
 
 @Controller
@@ -15,11 +18,31 @@ public class UsersController {
     @Autowired
     UsersService usersService;
 
+    @Autowired
+    private HttpServletRequest request;
+
     @ResponseBody
     @PostMapping("/login")
     public Map<String, Object> login(@RequestParam String username,
                                      @RequestParam String password) {
         return usersService.login(username, password);
+    }
+
+    @ResponseBody
+    @PostMapping("/logout")
+    public Map<String, Object> logout() {
+        String token=request.getHeader("x-token");
+        String[] n={token};
+        Map<String,Object> map=new HashMap<>();
+        Info info=new Info();
+        info.setAvatar("https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
+        map.put("code",20000);
+        info.setRoles(n);
+        info.setRole(token);
+        info.setName(token);
+        info.setAvatar("https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
+        map.put("data",info);
+        return map;
     }
 
     @ResponseBody
